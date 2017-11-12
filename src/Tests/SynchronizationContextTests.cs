6b24cb25faf82c823456df7192b0874268530e0f
Brandon Cuff <brandoncuff@gmail.com>
2017-11-12 10:28:11 +0100

    Added basic test

---
+++ src/Tests/SynchronizationContextTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using AspNetCoreCompatibility;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SynchronizationContextTests
+    {
+        [Test]
+        public Task HttpContext_should_live_across_await()
+        {
+            var target = new MockHttpContext();
+            Task t = null;
+
+            async Task Execute()
+            {
+                Assert.AreSame(target, HttpContextAccessor.Current);
+                await Task.Yield();
+                Assert.AreSame(target, HttpContextAccessor.Current);
+                await Task.Delay(1);
+                Assert.AreSame(target, HttpContextAccessor.Current);
+            }
+
+            new AspNetCompatibilitySynchronizationContext(target)
+                .ExecuteOrSchedule(() =>
+                {
+                    t = Execute();
+                });
+
+            return t;
+        }
+    }
+}
