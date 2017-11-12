b9db1c9440d01807c121a7ecaab5d24014d19e04
Brandon Cuff <brandoncuff@gmail.com>
2017-11-12 10:30:21 +0100

    Added xml doc

--- src/AspNetCoreCompatibility/MiddlewareExtensions.cs
+++ src/AspNetCoreCompatibility/MiddlewareExtensions.cs
@@ -6,6 +6,11 @@ namespace AspNetCoreCompatibility
 {
     public static class MiddlewareExtensions
     {
+        /// <summary>
+        /// Enables middleware that makes ASP .NET Core behave like the older ASP .NET.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <returns>The application builder.</returns>
         public static IApplicationBuilder UseAspNetCompatibility(this IApplicationBuilder app)
             => app.Use((context, next) =>
             {
